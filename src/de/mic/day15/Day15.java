42268f3212ffeb3bc56c77af8c3603177de62797
Thomas Michael <thomas.michael@god.de>
2022-12-15 21:23:26 +0100

    implmement every point

--- src/de/mic/day15/Day15.java
+++ src/de/mic/day15/Day15.java
@@ -1,6 +1,5 @@
 package de.mic.day15;
 
-import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Shape;
@@ -33,20 +32,28 @@ public class Day15 extends Solver {
 			all.add(createRect);
 			beacon.add(createBeacon(value));
 		}
-
-		int startX = beacon.stream().mapToInt(b -> b.x).min().getAsInt();
-		int endX = beacon.stream().mapToInt(b -> b.x).max().getAsInt();
+		;
+		int startX = Math.min(findStart(beacon), all.stream().mapToInt(s -> s.getBounds().x).min().getAsInt());
+		int endX = Math.max(findMax(beacon), all.stream().mapToInt(s -> s.getBounds().x).max().getAsInt());
 		int y = 10;
 		int count = 0;
 		for (int x = startX; x <= endX; x++) {
 			Point point = new Point(x, y);
+			boolean found = false;
 			for (Shape rec : all) {
 				if (rec.contains(point)) {
 					// is in
-					count++;
-					break;
+					if (!found) {
+
+						System.out.println("drin: " + point);
+						count++;
+						found = true;
+					} else {
+						System.out.println("2x drin: " + point);
+					}
 				} else {
 					// not in
+//					System.out.println("nicht drin: " + point);
 
 				}
 			}
@@ -61,6 +68,14 @@ public class Day15 extends Solver {
 		return "not solved: " + count;
 	}
 
+	private int findMax(Set<Point> beacon) {
+		return beacon.stream().mapToInt(b -> b.x).max().getAsInt();
+	}
+
+	private int findStart(Set<Point> beacon) {
+		return beacon.stream().mapToInt(b -> b.x).min().getAsInt();
+	}
+
 	private Shape createRect(String row) {
 
 		String[] split = row.split("=");
@@ -69,7 +84,6 @@ public class Day15 extends Solver {
 		Integer y1 = Integer.valueOf(split[2].split(":")[0]);
 		Integer x2 = Integer.valueOf(split[3].split(",")[0]);
 		Integer y2 = Integer.valueOf(split[4]);
-		Dimension dimension = new Dimension(x2, y2);
 
 		Integer radius = Math.abs(x1 - x2) + Math.abs(y1 - y2);
 
@@ -83,18 +97,17 @@ public class Day15 extends Solver {
 		List<Integer> allY = new ArrayList<>();
 		for (int i = -radius; i <= radius; i++) {
 			int x = x1 - i;
-			int y = radius + Math.abs(i);
+			int y = y1 + (radius - Math.abs(i));
 			allX.add(x);
 			allY.add(y);
 		}
+		// 3 -> -3
 		for (int i = (radius - 1); i > -radius; i--) {
 			int x = x1 + i;
-			int y = y1 - Math.abs(radius - i);
+			int y = y1 - (radius - Math.abs(-i));
 			allX.add(x);
 			allY.add(y);
 		}
-		System.out.println("Size x:" + allX.size());
-		System.out.println("Size y:" + allY.size());
 		if (!allX.contains(x2)) {
 			throw new RuntimeException("Beacle x not in " + x2);
 		}
@@ -102,8 +115,8 @@ public class Day15 extends Solver {
 			throw new RuntimeException("Beacle x not in " + x2);
 		}
 
-		int[] xx = allX.stream().mapToInt(i -> i).toArray();
-		int[] yy = allY.stream().mapToInt(i -> i).toArray();
+		int[] xx = allX.stream().mapToInt(x -> x).toArray();
+		int[] yy = allY.stream().mapToInt(x -> x).toArray();
 		int zzz = 4;
 
 		Shape theCircle = new Polygon(xx, yy, allX.size());
